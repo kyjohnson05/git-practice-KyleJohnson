fa0b64f2fcc5e3a4bed2e6561bcfb44bfcb28d48
Kyle Johnson <kjjohnson9@ualr.edu>
2025-02-19 16:26:33 -0600

    Run Code Analysis & Fix Warnings → Use Inspect Code (Code > Inspect Code)

--- assignments/PyCharm_Power_User_Challenge.py
+++ assignments/PyCharm_Power_User_Challenge.py
@@ -1,16 +1,16 @@
-def display(board):
+def display_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 5)
 
 
-def win(board, display):
+def win(board, player):
     for row_index in range(3):
-        if all(board[row_index][col_index] == display for col_index in range(3)) or all(
-                board[col_index][row_index] == display for col_index in range(3)):
+        if all(board[row_index][col_index] == player for col_index in range(3)) or all(
+                board[col_index][row_index] == player for col_index in range(3)):
             return True
-    if all(board[row_index][row_index] == display for row_index in range(3)) or all(
-            board[row_index][2 - row_index] == display for row_index in range(3)):
+    if all(board[row_index][row_index] == player for row_index in range(3)) or all(
+            board[row_index][2 - row_index] == player for row_index in range(3)):
         return True
     return False
 
@@ -19,26 +19,26 @@ def full(board):
     return all(column != " " for row in board for column in row)
 
 
-def turn():
+def play_game():
     board = [[" " for _ in range(3)] for _ in range(3)]
-    display = ["X", "O"]
+    display_team = ["X", "O"]
     print("Tic-Tac-Toe Game")
-    display(board)
-    for turn in range(9):
-        player = display[turn % 2]
+    display_board(board)
+    for turn_amount in range(9):
+        current_player = display_team[turn_amount % 2]
         while 1:
             try:
-                row, column = map(int, input(f"P {player}, row col (0-2): ").split())
+                row, column = map(int, input(f"P {current_player}, row col (0-2): ").split())
                 if board[row][column] == " ":
-                    board[row][column] = player
+                    board[row][column] = current_player
                     break
                 else:
                     print("Nope. Again.")
-            except:
+            except ValueError:
                 print("Wrong. 0-2 pls.")
-        display(board)
-        if win(board, player):
-            print(f"P {player} wins!")
+        display_board(board)
+        if win(board, current_player):
+            print(f"P {current_player} wins!")
             return
         if full(board):
             print("Draw!")
@@ -46,4 +46,4 @@ def turn():
     print("Draw!")
 
 
-turn()
+play_game()
