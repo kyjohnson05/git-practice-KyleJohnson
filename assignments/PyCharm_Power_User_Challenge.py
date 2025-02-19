81a2e62be9e2a3dec3fc052d76a0402a3b52c040
Kyle Johnson <157499502+kyjohnson05@users.noreply.github.com>
2025-02-19 15:44:17 -0600

    Update PyCharm_Power_User_Challenge.py
    Rename stuff and make it better → Use Refactor > Rename (Shift + F6)

--- assignments/PyCharm_Power_User_Challenge.py
+++ assignments/PyCharm_Power_User_Challenge.py
@@ -1,47 +1,47 @@
-def p(b):
- for r in b:
-  print(" | ".join(r))
-  print("-"*5)
- 
- 
-def c_w(b,p):
- for i in range(3):
-  if all(b[i][j]==p for j in range(3)) or all(b[j][i]==p for j in range(3)):
-   return True
- if all(b[i][i]==p for i in range(3)) or all(b[i][2-i]==p for i in range(3)):
-  return True
- return False
- 
- 
-def f(b):
- return all(c!=" " for r in b for c in r)
- 
- 
-def t():
- b=[[" "for _ in range(3)]for _ in range(3)]
- p=["X","O"]
- print("Tic-Tac-Toe Game")
- p(b)
- for t in range(9):
-  pl=p[t%2]
-  while 1:
-   try:
-    r,c=map(int,input(f"P {pl}, row col (0-2): ").split())
-    if b[r][c]==" ":
-     b[r][c]=pl
-     break
-    else:
-     print("Nope. Again.")
-   except:
-    print("Wrong. 0-2 pls.")
-  p(b)
-  if c_w(b,pl):
-   print(f"P {pl} wins!")
-   return
-  if f(b):
-   print("Draw!")
-   return
- print("Draw!")
- 
- 
-t() 
+def display(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+
+def win(board, display):
+    for row_index in range(3):
+        if all(board[row_index][col_index] == display for col_index in range(3)) or all(board[col_index][row_index] == display for col_index in range(3)):
+            return True
+    if all(board[row_index][row_index] == display for row_index in range(3)) or all(board[row_index][2 - row_index] == display for row_index in range(3)):
+        return True
+    return False
+
+
+def full(board):
+    return all(column != " " for row in board for column in row)
+
+
+def turn():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    display = ["X", "O"]
+    print("Tic-Tac-Toe Game")
+    display(board)
+    for turn in range(9):
+        player = display[turn % 2]
+        while 1:
+            try:
+                row, column = map(int, input(f"P {player}, row col (0-2): ").split())
+                if board[row][column] == " ":
+                    board[row][column] = player
+                    break
+                else:
+                    print("Nope. Again.")
+            except:
+                print("Wrong. 0-2 pls.")
+        display(board)
+        if win(board, player):
+            print(f"P {player} wins!")
+            return
+        if full(board):
+            print("Draw!")
+            return
+    print("Draw!")
+
+
+turn()
