7a5362c5000ee6bf216e4f03bf6b1eb3d5d2a789
Kyle Johnson <157499502+kyjohnson05@users.noreply.github.com>
2025-04-06 23:05:58 -0500

    Simplified slow_func using list comprehension

--- analysis_refactor_assignment/main.py
+++ analysis_refactor_assignment/main.py
@@ -6,10 +6,7 @@ def expensive_op(n):
 
 @profile
 def slow_func(lst):
-    result = []
-    for i in range(len(lst)):
-        result.append(expensive_op(i))
-    return result
+    return [expensive_op(i) for i in range(len(lst))]
  
 def main():
     numbers = list(range(1000))
